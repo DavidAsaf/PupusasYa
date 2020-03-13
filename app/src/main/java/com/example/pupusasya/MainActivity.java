07d08aa4be36101a646f877c79a9656fdc8714f7
eve9520 <62076716+eve9520@users.noreply.github.com>
2020-03-13 00:21:02 -0600

    menu y pantallas listas

--- app/src/main/java/com/example/pupusasya/MainActivity.java
+++ app/src/main/java/com/example/pupusasya/MainActivity.java
@@ -1,14 +1,77 @@
 package com.example.pupusasya;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    } // mensaje para alguien
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.abrir_navegacion, R.string.cerrar_navegacion);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        if(savedInstanceState==null){
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment,
+                    new BienvenidaFragmento()).commit();
+            navigationView.setCheckedItem(R.id.nav_bienvenida);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.nav_bienvenida:
+                getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment,
+                        new BienvenidaFragmento()).commit();
+                break;
+            case R.id.nav_inscripcion:
+                getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment,
+                        new InscripcionFragmento()).commit();
+                break;
+            case R.id.nav_restaurante:
+                getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment,
+                        new PupuseriasFragmento()).commit();
+                break;
+            case R.id.nav_historial:
+                getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment,
+                        new HistorialFragmento()).commit();
+                break;
+            case R.id.nav_chat:
+                getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment,
+                        new ChatFragmento()).commit();
+                break;
+            case R.id.nav_salir:
+                Toast.makeText(this, "Salir", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+        }
 }
