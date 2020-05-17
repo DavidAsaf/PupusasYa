ad7e608d7d7aa197a9dad51a6922dfc6a7e04fb2
david.amaya <2014AR603@catolica.edu.sv>
2020-05-16 20:06:57 -0500

    Avanzando un 7%

---
+++ app/src/main/java/com/example/pupusasya/ListaPupusasFragment.java
@@ -0,0 +1,28 @@
+package com.example.pupusasya;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ListaPupusasFragment extends Fragment {
+
+    public ListaPupusasFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_lista_pupusas, container, false);
+    }
+}
