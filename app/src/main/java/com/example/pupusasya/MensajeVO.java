8eca69d6cc7ee7914e1055cabe4e0b2b905e77cf
Karen Rocío Erazo <rozerazo@gmail.com>
2020-05-28 21:06:25 -0600

    realizando pruebas de chat------ no puedo acceder a la app para ver el chat jajajajaj

---
+++ app/src/main/java/com/example/pupusasya/MensajeVO.java
@@ -0,0 +1,23 @@
+package com.example.pupusasya;
+
+public class MensajeVO {
+
+    private String name;
+    private String message;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
