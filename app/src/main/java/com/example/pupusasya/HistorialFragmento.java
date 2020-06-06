cfa64db85feef1b221fc5f04195c7ea7705879d6
Karen Rocío Erazo <rozerazo@gmail.com>
2020-06-05 21:26:38 -0600

    revisar códigooo....

--- app/src/main/java/com/example/pupusasya/HistorialFragmento.java
+++ app/src/main/java/com/example/pupusasya/HistorialFragmento.java
@@ -1,49 +1,30 @@
 package com.example.pupusasya;
 
-import android.app.ProgressDialog;
-import android.content.ContentValues;
-import android.content.Context;
-import android.content.Intent;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.PrecomputedText;
-import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.BaseAdapter;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-import androidx.navigation.fragment.NavHostFragment;
-
-import com.example.pupusasya.Clases.Conexion;
-import com.example.pupusasya.Clases.pupuseriaDB;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 
 import org.json.JSONArray;
-import org.json.JSONException;
 
 import java.util.ArrayList;
 
 import cz.msebera.android.httpclient.Header;
 
-import static android.content.Context.LAYOUT_INFLATER_SERVICE;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 public class HistorialFragmento extends Fragment {
     View vista;
-
+    ListView listado;
     Button boton;
 
     @Nullable
@@ -51,33 +32,64 @@ public class HistorialFragmento extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         vista = inflater.inflate(R.layout.fragmento_historial, container, false);
-        boton = vista.findViewById(R.id.btnHistorial);
-        boton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Fragment fragment= new HistorialFragmento();
-                FragmentManager fragmentManager=getActivity().getSupportFragmentManager();
-                FragmentTransaction fragmentTransaction=fragmentManager.beginTransaction();
-                fragmentTransaction.replace(R.id.nav_host_fragment,fragment);
-                fragmentTransaction.addToBackStack(null);
-                fragmentTransaction.commit();
+        //boton = vista.findViewById(R.id.btnMostrarHist);
+        //listado= (ListView) inflater.inflate((R.layout.fragmento_historial), container, false);
+        listado= vista.findViewById(R.id.lsHistorial);
 
+        ObtenerDatos();
+
+       return vista;
+
+
+    }
+    public void ObtenerDatos() {
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        String url = "http://localhost/pupusasya/historialPupuseria.php";
+
+        RequestParams parametros = new RequestParams();
+
+
+        client.post(url, parametros, new AsyncHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                if (statusCode == 200) {
+                    CargaLista(obtenerDatosJSON(new String(responseBody)));
+                }
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
 
             }
         });
-
-        return vista;
-
-
-
-
-
+    }
+    public void CargaLista(ArrayList<String> datos) {
+        ArrayAdapter adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, datos);
+        listado.setAdapter(adapter);
     }
 
-
-
-
+    public ArrayList<String> obtenerDatosJSON(String response) {
+        ArrayList<String> listado = new ArrayList<>();
+        try {
+            JSONArray jsonArray = new JSONArray(response);
+            String texto;
+            for (int i = 0; i < jsonArray.length(); i++) {
+                texto = jsonArray.getJSONObject(i).getString("IdHistorial") + " " +
+                        jsonArray.getJSONObject(i).getString("Detalle") + " " +
+                        jsonArray.getJSONObject(i).getString("PrecioTotal") + " " +
+                        jsonArray.getJSONObject(i).getString("Delivery") + " " +
+                        jsonArray.getJSONObject(i).getString("Nombre") + " " +
+                        jsonArray.getJSONObject(i).getString("Estado") + " ";
+                listado.add(texto);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } return listado;
     }
+
+        }
 
 
 
