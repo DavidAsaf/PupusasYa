163fd326a8ba6be58395d0e71727319563ee26e8
Karen Rocío Erazo <rozerazo@gmail.com>
2020-06-13 14:38:38 -0600

    finalizando detalle de compra

---
+++ app/src/main/java/com/example/pupusasya/Clases/DetCompraDB.java
@@ -0,0 +1,23 @@
+package com.example.pupusasya.Clases;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DetCompraDB extends SQLiteOpenHelper {
+
+    public DetCompraDB (Context context, String name, SQLiteDatabase.CursorFactory factory, int version){
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE detalleSelected (idHistorial integer)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS detalleSelected");
+        db.execSQL("CREATE TABLE detalleSelected (idHistorial integer)");
+    }
+}
--- app/src/main/java/com/example/pupusasya/DetalleCompra.java
+++ app/src/main/java/com/example/pupusasya/DetalleCompra.java
@@ -1,33 +1,22 @@
 package com.example.pupusasya;
 
-
 import android.app.ProgressDialog;
-import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.PrecomputedText;
-import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 import com.example.pupusasya.Clases.Conexion;
-import com.example.pupusasya.Clases.pupuseriaDB;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
@@ -41,14 +30,11 @@ import cz.msebera.android.httpclient.Header;
 
 import static android.content.Context.LAYOUT_INFLATER_SERVICE;
 
-public class HistorialFragmento extends Fragment {
+public class DetalleCompra extends Fragment {
+
     private ListView lista;
-    private static ArrayList Detalle;
-    private ArrayList IdHistorial;
-    private static ArrayList PrecioTotal;
-    private static ArrayList Delivery;
+    private static ArrayList Cantidad;
     private static ArrayList Nombre;
-    private static ArrayList Estado;
 
     private Intent intent;
     View vista;
@@ -58,48 +44,31 @@ public class HistorialFragmento extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        vista= inflater.inflate(R.layout.fragmento_historial, container, false);
+        vista= inflater.inflate(R.layout.fragment_detalle_compra, container, false);
 
-        lista = (ListView) vista.findViewById(R.id.lstPupusas);
+        lista = (ListView) vista.findViewById(R.id.lstCompra);
 
-        IdHistorial = new ArrayList();
-        //Detalle = new ArrayList();
-        PrecioTotal = new ArrayList();
-        Delivery = new ArrayList();
+        Cantidad= new ArrayList();
         Nombre = new ArrayList();
-        Estado = new ArrayList();
 
-        //View view = inflater.inflate(R.layout.detallehist, container, false);
 
-        Button btnLanzarActivity = (Button) vista.findViewById(R.id.btnDetalleCom);
-        btnLanzarActivity.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), DetalleCompra.class);
-                startActivity(intent);
-            }
-        });
-
-        cargarHistorial();
+        cargarDetalle();
 
 
         return vista;
     }
 
-    public void cargarHistorial(){
-        IdHistorial.clear();
-        //Detalle.clear();
-        PrecioTotal.clear();
-        Delivery.clear();
+    public void cargarDetalle(){
+        Cantidad.clear();
         Nombre.clear();
-        Estado.clear();
+
 
         final ProgressDialog progressDialog = new ProgressDialog(getActivity());
         progressDialog.setMessage("Cargando Datos...");
         progressDialog.show();
         AsyncHttpClient client = new AsyncHttpClient();
         Conexion connect = new Conexion();
-        String url = connect.getUrlDireccion() +"historialPupuseria.php";
+        String url = connect.getUrlDireccion() +"detalleCompra.php";
         RequestParams parametros = new RequestParams();
 
         client.get(url, parametros, new AsyncHttpResponseHandler() {
@@ -111,19 +80,14 @@ public class HistorialFragmento extends Fragment {
 
                         JSONArray jsonArray = new JSONArray(new String(responseBody));
                         for (int i = 0; i < jsonArray.length(); i++) {
-                            IdHistorial.add(jsonArray.getJSONObject(i).getString("IdHistorial"));
-                           // Detalle.add(jsonArray.getJSONObject(i).getString("Detalle"));
-                            PrecioTotal.add(jsonArray.getJSONObject(i).getString("PrecioTotal"));
-                            Delivery.add(jsonArray.getJSONObject(i).getString("Delivery"));
+                            Cantidad.add(jsonArray.getJSONObject(i).getString("Cantidad"));
                             Nombre.add(jsonArray.getJSONObject(i).getString("Nombre"));
-                            Estado.add(jsonArray.getJSONObject(i).getString("Estado"));
-                            //Toast.makeText(getContext(), jsonArray.getJSONObject(i).getString("Nombre"), Toast.LENGTH_LONG).show();
-                        }
+                              }
 
-                        lista.setAdapter(new HistorialFragmento.CustonAdater(getActivity().getApplicationContext(),  PrecioTotal, Delivery, Nombre, Estado));
+                        lista.setAdapter(new DetalleCompra.CustonAdater(getActivity().getApplicationContext(),Cantidad, Nombre));
 
-                        final HistorialFragmento.CustonAdater CustonAdater =
-                                new HistorialFragmento.CustonAdater(HistorialFragmento.this.getActivity().getApplicationContext(),  PrecioTotal, Delivery, Nombre, Estado);
+                        final DetalleCompra.CustonAdater CustonAdater =
+                                new CustonAdater(DetalleCompra.this.getActivity().getApplicationContext(),  Cantidad, Nombre);
                         lista.setAdapter(CustonAdater);
                         progressDialog.dismiss();
 
@@ -152,10 +116,10 @@ public class HistorialFragmento extends Fragment {
     static class CustonAdater extends BaseAdapter {
         Context ctx;
         LayoutInflater layoutInflater;
-        TextView  etPrecioTo, etid, etDelivery, etNombre, etEstado;
-        Button btnSelect;
+        TextView etCantidad, etNombre;
 
-        public CustonAdater(Context applicationContext, ArrayList PrecioTotal, ArrayList Delivery, ArrayList Nombre, ArrayList Estado) {
+
+        public CustonAdater(Context applicationContext, ArrayList Cantidad, ArrayList Nombre) {
             this.ctx = applicationContext;
             layoutInflater = (LayoutInflater) this.ctx.getSystemService(LAYOUT_INFLATER_SERVICE);
 
@@ -163,7 +127,7 @@ public class HistorialFragmento extends Fragment {
 
         @Override
         public int getCount() {
-            return Nombre.size();
+            return Cantidad.size();
         }
 
         @Override
@@ -178,18 +142,20 @@ public class HistorialFragmento extends Fragment {
 
         @Override
         public View getView(int position, View converView, ViewGroup parent) {
-            ViewGroup viewGroup1 = (ViewGroup) layoutInflater.inflate(R.layout.fragmento_historial, null);
+            ViewGroup viewGroup1 = (ViewGroup) layoutInflater.inflate(R.layout.detallehist, null);
 
 
             //etDetalle.setText(Detalle.get(position).toString());
-            etPrecioTo.setText(PrecioTotal.get(position).toString());
-            etDelivery.setText(Delivery.get(position).toString());
+            etCantidad.setText(Cantidad.get(position).toString());
             etNombre.setText(Nombre.get(position).toString());
-            etEstado.setText(Estado.get(position).toString());
-            return viewGroup1;
+
+                      return viewGroup1;
         }
     }
 }
+
+
+
 
 
 
